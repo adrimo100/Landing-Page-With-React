648be49fedd17eb1d2ed4ab49c68673cc04195fc
Adrián Molina Elvira <adrimo100@gmail.com>
2022-02-11 17:00:06 +0000

    Ejercicio acabado

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className="d-flex justify-content-center align-items-center py-4 bg-dark text-white">
+		<footer
+			id="foot"
+			className="d-flex justify-content-center align-items-center py-4 bg-dark text-white">
 			copyright @ Your Website 2022
 		</footer>
 	);
